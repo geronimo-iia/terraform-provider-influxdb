4f6c18ea4d883e463a5e22e19387b6e7db235757
drfaust92 <ilia.lazebnik@gmail.com>
2022-01-24 22:29:03 +0200

    tests

--- influxdb/provider_test.go
+++ influxdb/provider_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 // To run these acceptance tests, you will need an InfluxDB server.
@@ -15,22 +14,22 @@ import (
 // To run the tests against a remote InfluxDB server, set the INFLUXDB_URL,
 // INFLUXDB_USERNAME and INFLUXDB_PASSWORD environment variables.
 
-var testAccProviders map[string]terraform.ResourceProvider
+var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
 
 func init() {
-	testAccProvider = Provider().(*schema.Provider)
-	testAccProviders = map[string]terraform.ResourceProvider{
+	testAccProvider = Provider()
+	testAccProviders = map[string]*schema.Provider{
 		"influxdb": testAccProvider,
 	}
 }
 
 func TestProvider(t *testing.T) {
-	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
 
 func TestProvider_impl(t *testing.T) {
-	var _ terraform.ResourceProvider = Provider()
+	var _ *schema.Provider = Provider()
 }
